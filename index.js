d66dc87be23d69d89e570d9be851290ae2c6e2b3
Tariq <tic084@gmail.com>
2019-05-01 15:49:22 -0400

    Only move to new page when last is successful

--- index.js
+++ index.js
@@ -9,25 +9,34 @@ db.defaults({ movies: [] }).write();
 const moviesTable = db.get('movies');
 
 let page = 0;
-const isRunning = true;
 
 const getNextPage = () => {
   console.log(`Retrieving page ${page}`);
   getMovieList(page).then((movieList) => {
     if (!movieList.length) {
-      isRunning = false;
-      console.warn(`No movies returned. Stopping parsing at page ${page - 1}`);
+      console.warn(`No movies returned. Stopping parsing at page ${page}`);
       return;
     }
-    movieList.forEach(({ name, url, score, date }) => {
-      const existingMovie = moviesTable.find({ url }).value();
-      if (!existingMovie) {
-        getMovieDetails(url).then(({ genres }) => {
-          moviesTable.push({ name, score, url, genres, date }).write();
+
+    const newMovieList = movieList.filter(({ url }) => !moviesTable.find({ url }).value());
+    let successCount = 0;
+    let completedCount = 0;
+    newMovieList.forEach(({ name, url, score, date }) => {
+      getMovieDetails(url).then(({ genres }) => {
+        moviesTable.push({ name, score, url, genres, date }).write();
+        successCount++;
+        completedCount++;
+
+        if (completedCount === newMovieList.length && successCount !== completedCount) { // All movies attempted. Some errors
+          getNextPage();
+        } else if (completedCount === newMovieList.length && successCount === completedCount) { // All movies attempted. No errors
           page++;
           getNextPage();
-        }).catch(err => console.log(err));;
-      }
+        }
+      }).catch(err => {
+        completedCount++;
+        console.log(url, err);
+      });
     });
   }).catch(err => console.log(err));
 };
