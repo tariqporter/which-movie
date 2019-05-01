730ad77f4ba9b06931c1ee9df7299e14f75ef9ff
Tariq <tic084@gmail.com>
2019-05-01 15:09:29 -0400

    Loop pages for all movies

--- index.js
+++ index.js
@@ -8,13 +8,28 @@ const db = low(adapter);
 db.defaults({ movies: [] }).write();
 const moviesTable = db.get('movies');
 
-getMovieList(0).then((movieList) => {
-  movieList.forEach(({ name, url, score, date }) => {
-    const existingMovie = moviesTable.find({ url }).value();
-    if (!existingMovie) {
-      getMovieDetails(url).then(({ genres }) => {
-        moviesTable.push({ name, score, url, genres, date }).write();
-      }).catch(err => console.log(err));;
+let page = 0;
+const isRunning = true;
+
+const getNextPage = () => {
+  console.log(`Retrieving page ${page}`);
+  getMovieList(page).then((movieList) => {
+    if (!movieList.length) {
+      isRunning = false;
+      console.warn(`No movies returned. Stopping parsing at page ${page - 1}`);
+      return;
     }
-  });
-}).catch(err => console.log(err));
+    movieList.forEach(({ name, url, score, date }) => {
+      const existingMovie = moviesTable.find({ url }).value();
+      if (!existingMovie) {
+        getMovieDetails(url).then(({ genres }) => {
+          moviesTable.push({ name, score, url, genres, date }).write();
+          page++;
+          getNextPage();
+        }).catch(err => console.log(err));;
+      }
+    });
+  }).catch(err => console.log(err));
+};
+
+getNextPage();
